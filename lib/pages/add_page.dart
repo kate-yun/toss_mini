5aeb4650e523832086790de126ca34bc79db77f8
MyunghyunYeo <wolfmaeng@gmail.com>
2022-10-06 18:21:56 +0900

    [first1] very early view (no style)

--- lib/pages/add_page.dart
+++ lib/pages/add_page.dart
@@ -1,4 +1,7 @@
+import 'package:chang_mini/config/colors.dart';
+import 'package:chang_mini/pages/send_page.dart';
 import 'package:chang_mini/services/bank_service.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -16,7 +19,95 @@ class _AddPageState extends State<AddPage> {
       builder: (context, service, child) {
         return GestureDetector(
           onTap: () {},
-          child: Scaffold(),
+          child: Scaffold(
+            backgroundColor: TossColor.white,
+            appBar: AppBar(
+              backgroundColor: TossColor.grey1,
+              elevation: 0,
+              leading: IconButton(
+                onPressed: () {
+                  Navigator.pop(context); // 작업을 위한 임시 뒤로가기
+                },
+                icon: Icon(
+                  Icons.arrow_back_ios,
+                  color: TossColor.bluegrey,
+                ),
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SendPage(),
+                      ),
+                    );
+                  },
+                  child: Text('확인'),
+                ),
+              ],
+            ),
+            body: SafeArea(
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 24,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '은행 추가하기',
+                      style: TextStyle(
+                        color: TossColor.black,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Row(
+                      children: [
+                        Text('은행명'),
+                        Expanded(child: CupertinoTextField.borderless()),
+                        Text('은행'),
+                      ],
+                    ),
+                    SizedBox(height: 4),
+                    Row(
+                      children: [
+                        Text('잔액'),
+                        Expanded(child: CupertinoTextField.borderless()),
+                        Text('원'),
+                      ],
+                    ),
+                    Spacer(),
+                    Container(
+                      color: TossColor.white,
+                      height: 200,
+                      width: double.infinity,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset('assets/toss_bank.png'),
+                          Text("토스 은행"),
+                          Text("비대면으로 통장을 개설할 수 있어요"),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: Text("지금 바로 만들기"),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 32),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('확인'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         );
       },
     );
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:chang_mini/components/bank_container.dart';
+import 'package:chang_mini/config/colors.dart';
+import 'package:chang_mini/pages/add_page.dart';
 import 'package:chang_mini/services/bank_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -15,7 +18,83 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Consumer<BankService>(
       builder: (context, service, child) {
-        return Scaffold();
+        return Scaffold(
+          appBar: AppBar(
+            automaticallyImplyLeading: false, // 뒤로가기 버튼 제거
+            backgroundColor: TossColor.grey1,
+            elevation: 0,
+            title: Image.asset(
+              'assets/toss_logo.png',
+              height: 50,
+            ),
+            centerTitle: false, // title 가운데 정렬 해제
+            actions: [
+              Image.asset(
+                'assets/toss_appbar_icon_1.png',
+                height: 48,
+                width: 48,
+              ),
+              SizedBox(
+                width: 16,
+              ),
+              Image.asset(
+                'assets/toss_appbar_icon_2.png',
+                height: 36,
+                width: 36,
+              ),
+              SizedBox(
+                width: 16,
+              ),
+              Image.asset(
+                'assets/toss_appbar_icon_3.png',
+                height: 30,
+                width: 30,
+              ),
+              SizedBox(
+                width: 16,
+              ),
+            ],
+          ),
+          body: Container(
+            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: ListView(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: TossColor.grey1,
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text('자산'),
+                          Spacer(),
+                          IconButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      AddPage(), // AddPage 작업을 위한 임시 페이지 이동.
+                                ),
+                              );
+                            },
+                            icon: Icon(Icons.add),
+                          ),
+                        ],
+                      ),
+                      ...service.myBankList.map(
+                        (e) => BankContainer(bank: e),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
       },
     );
   }
